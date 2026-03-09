7578baebb3b4ad95fac2149ce0fbc71545651abe
Iraitz <iraitz.maritxalar@alumni.mondragon.edu>
2026-03-09 12:30:30 +0100

    pokemon list egiten

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
+let currentCss;
+
 function loadPage(page) {
   fetch(`../html/${page}`)
     .then(res => res.text())
     .then(html => {
       document.getElementById("content").innerHTML = html;
+
+      if(currentCss) currentCss.remove();
+
+      const cssPath = `../css/${page.replace(".html", ".css")}`;
+      fetch(cssPath).then(res => {
+        if(res.ok) {
+          const link = document.createElement("link");
+          link.rel = "stylesheet";
+          link.href = cssPath;
+          document.head.appendChild(link);
+          currentCss = link;
+        }
+      });
     });
 }
 
